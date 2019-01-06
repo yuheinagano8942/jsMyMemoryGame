b40e91c5759c49b1a6cf958452f18327bbd2dfc6
yuhei.nagano <yuheinagano8942@gmail.com>
2019-01-06 10:11:13 +0000

    first commit10

--- main.js
+++ main.js
@@ -2,12 +2,20 @@
   'use strict';
   
   var pairs = 2;
+  var cards = [];
   
   function init() {
     var i;
+    var card;
     for (i = 1; i <= pairs; i++) {
-      document.getElementById('stage').appendChild(createCard(i));
-      document.getElementById('stage').appendChild(createCard(i));
+      cards.push(createCard(i));
+      cards.push(createCard(i));
+      // document.getElementById('stage').appendChild(createCard(i));
+      // document.getElementById('stage').appendChild(createCard(i));
+    }
+    while (cards.length) {
+      card = cards.splice(Math.floor(Math.random() * cards.length), 1)[0];
+      document.getElementById('stage').appendChild(card);
     }
   }
   
